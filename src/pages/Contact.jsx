04f5eeda2eeb5d5e74c905ce5c01fdfbf2f06290
guilherme Angelo <guilhermeangelo@iesb.edu.br>
2024-10-21 10:53:39 -0300

    rotas

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+    return <div>
+       <h1>Contact Me</h1>
+       <p>Tente contactar com agente para descobrir suas duvidas.</p>
+
+    </div>
+  };
+  
+  export default Contact;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return <div>
+      <h1>Home</h1>
+
+      <p>Bem vindo a pagina inicial.</p>
+      <p>aqui temos o meu primeiro site</p>
+      
+      </div>
+     
+  };
+  
+  export default Home;
